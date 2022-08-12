518c6aa058cfe7bbd3be9313a4fcf3c644a18023
Noel Masur <noel.masur@atos.net>
2022-08-12 14:42:27 +0200

    Add second system out

--- Java/src/main/java/mro/arcade/game/ArcadeGameMain.java
+++ Java/src/main/java/mro/arcade/game/ArcadeGameMain.java
@@ -3,7 +3,9 @@ package mro.arcade.game;
 public class ArcadeGameMain {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("Hello World 2");
     }
 
 }
